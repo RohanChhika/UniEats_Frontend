0754779b0d8475ba2cc928efe3fcc9aa3de3f4c4
RohanChhika <2543404@students.wits.ac.za>
2024-09-05 09:58:23 +0200

    profile

--- src/components/buttons/LoginLogoutButton.js
+++ src/components/buttons/LoginLogoutButton.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from 'react-router-dom';
 
 const LoginLogoutButton = () => {
   const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
@@ -7,7 +8,9 @@ const LoginLogoutButton = () => {
   if (isAuthenticated){
     return (
       <>
+      <Link to={`/profile`}>
       <button className="button">View my Profile</button> 
+       </Link>
       <button className="button" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}> Log Out</button>
       </>
     );
